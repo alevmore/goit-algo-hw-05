8cdba1074b15d5c1e90b3e24be1f0bf73d594eef
Alev More <64901448+alevmore@users.noreply.github.com>
2024-11-06 00:45:18 +0200

    Update and rename homework_5_2.py to reduce.py

--- reduce.py
+++ reduce.py
@@ -15,7 +15,7 @@ def sum_profit(text: str, func: Callable):
 
 if __name__=="__main__":
 
-    text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+    text = "The employee's total income consists of several parts: 1000.01 as the main income, supplemented by additional income of 27.45 and 324.00 dollars."
     total_income = sum_profit(text, generator_numbers)
     print(f"Загальний дохід: {total_income}")
 
